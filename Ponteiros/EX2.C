dbb57b05af3fb2987e546e3137cb3eb304946fc0
Jefferson korte junior <jeffersomkorte@gmail.com>
2025-04-05 09:09:07 -0300

    Update EX2.C

--- Ponteiros/EX2.C
+++ Ponteiros/EX2.C
@@ -12,8 +12,6 @@ p = &y; //P recebe o endereço de y
 x = *p; //X recebe o valor de y, que é 0
 (*p)++; //O valor de Y é incrementado em 1, então y = 1
 x = 4; //X recebe 4
-
-(*p)++; //Icrementa o valor no endereço apontado, entao Y = 2
 x--; //Aqui X é decrementado entao passa a ter valor 3
 (*p) += x; //Aqui o valor de Y que P esta apontando é incrementado a X que vale 3, entao Y = 5
 
